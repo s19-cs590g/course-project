ce15b8534410d74b95016b5da28a577ef411a86d
Blake Geraci <bgeraci851@hcc.edu>
2019-04-30 21:20:27 -0400

    adding some scene changing and menus

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//original source from https://answers.unity.com/questions/1432380/im-trying-to-toggle-something.html
+public class PauseController : MonoBehaviour
+{
+    public Transform canvas;
+
+    // Update is called once per frame
+    void Start() {
+        canvas.gameObject.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+    }
+
+    public void Pause() {
+        if (canvas.gameObject.activeInHierarchy == false) {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
